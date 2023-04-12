9194f3ddc34efce8f8acc6b708336e8c08cb62f0
unknown <shen.zek@northeastern.edu>
2023-04-11 22:03:28 -0400

    3-3 Add functions that acquire the utility value of chess.

--- src/utility/ChessValueUtil.java
+++ src/utility/ChessValueUtil.java
@@ -1,6 +1,6 @@
 package utility;
 
-import components.IBoard;
+import components.BlackOrWhite;
 import components.Pos;
 
 import java.util.ArrayList;
@@ -77,7 +77,138 @@ public class ChessValueUtil {
         whiteChessLinkToValue.put("*+++*", 10);
     }
 
+    public static List<List<String>> getSettedLocationList(String[][] board, BlackOrWhite currentPlayer,
+                                                           Pos pos, int length, int loc) {
+        int row = pos.row;
+        int col = pos.col;
 
+        List<List<String>> list = new ArrayList<>();
+
+        String currentPlayerChess = currentPlayer == BlackOrWhite.BLACK ? "#" : "*";
+        String opponentPlayerChess = currentPlayer == BlackOrWhite.BLACK ? "*" : "#";
+
+        Pos layStart = new Pos(row, col - loc + 1);
+        Pos layEnd = new Pos(row, col + length - loc);
+        int LayStartCol = layStart.col;
+        int LayEndCol = layEnd.col;
+
+        if (inBoard(layStart) && inBoard(layEnd)) {
+            List<String> attackList = new ArrayList<>();
+            List<String> defendList = new ArrayList<>();
+
+            for (int i = LayStartCol; i <= LayEndCol; i++) {
+                if (i == col) {
+                    attackList.add(currentPlayerChess);
+                } else {
+                    attackList.add(board[row][i]);
+                }
+            }
+
+            for (int i = LayStartCol; i <= LayEndCol; i++) {
+                if (i == col) {
+                    defendList.add(opponentPlayerChess);
+                } else {
+                    defendList.add(board[row][i]);
+                }
+            }
+
+            list.add(attackList);
+            list.add(defendList);
+        }
+
+        Pos standStart = new Pos(row - loc + 1, col);
+        Pos standEnd = new Pos(row + length - loc, col);
+        int StandStartRow = standStart.row;
+        int StandEndRow = standEnd.row;
+
+        if (inBoard(standStart) && inBoard(standEnd)) {
+            List<String> attackList = new ArrayList<>();
+            List<String> defendList = new ArrayList<>();
+
+            for (int i = StandStartRow; i <= StandEndRow; i++) {
+                if (i == row) {
+                    attackList.add(currentPlayerChess);
+                } else {
+                    attackList.add(board[i][col]);
+                }
+            }
+            for (int i = StandStartRow; i <= StandEndRow; i++) {
+                if (i == row) {
+                    defendList.add(opponentPlayerChess);
+                } else {
+                    defendList.add(board[i][col]);
+                }
+            }
+            list.add(attackList);
+            list.add(defendList);
+        }
+
+        Pos MainStart = new Pos(row - loc + 1, col - loc + 1);
+        Pos MainEnd = new Pos(row + length - loc, col + length - loc);
+
+        int MainStartRow = MainStart.row;
+        int MainEndRow = MainEnd.row;
+        int MainStartCol = MainStart.col;
+
+        if (inBoard(MainEnd) && inBoard(MainStart)) {
+            List<String> attackList = new ArrayList<>();
+            List<String> defendList = new ArrayList<>();
+
+            for (int rowi = MainStartRow, coli = MainStartCol; rowi <= MainEndRow; rowi++, coli++) {
+                if (rowi == row && coli == col) {
+                    attackList.add(currentPlayerChess);
+                } else {
+                    attackList.add(board[rowi][coli]);
+                }
+            }
+            for (int rowi = MainStartRow, coli = MainStartCol; rowi <= MainEndRow; rowi++, coli++) {
+                if (rowi == row && coli == col) {
+                    defendList.add(opponentPlayerChess);
+                } else {
+                    defendList.add(board[rowi][coli]);
+                }
+            }
+            list.add(attackList);
+            list.add(defendList);
+        }
+
+        Pos ViceStart = new Pos(row + loc - 1, col - loc + 1);
+        Pos ViceEnd = new Pos(row - length + loc, col + length - loc);
+
+        int ViceStartRow = ViceStart.row;
+        int ViceEndRow = ViceEnd.row;
+        int ViceStartCol = ViceStart.col;
+        if (inBoard(ViceStart) && inBoard(ViceEnd)) {
+
+            List<String> attackList = new ArrayList<>();
+            List<String> defendList = new ArrayList<>();
+
+            for (int rowi = ViceStartRow, coli = ViceStartCol; rowi >= ViceEndRow; rowi--, coli++) {
+                if (rowi == row && coli == col) {
+                    attackList.add(currentPlayerChess);
+                } else {
+                    attackList.add(board[rowi][coli]);
+                }
+            }
+
+            for (int rowi = ViceStartRow, coli = ViceStartCol; rowi >= ViceEndRow; rowi--, coli++) {
+                if (rowi == row && coli == col) {
+                    defendList.add(opponentPlayerChess);
+                } else {
+                    defendList.add(board[rowi][coli]);
+                }
+            }
+
+            list.add(attackList);
+            list.add(defendList);
+        }
+
+        return list;
+    }
+
+    private static boolean inBoard(Pos p) {
+        return p.row >= 0 && p.row < 15 && p.col >= 0 && p.col < 15;
+    }
 
     private static boolean assignPositionValue(Pos chessPosition, List<String> chessLink, String[][] board) {
         int value = 0;
