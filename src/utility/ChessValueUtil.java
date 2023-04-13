6e19756fa7919d256b3f10731290601fb7799405
ruaruarua <shen.zek@northeastern.edu>
2023-04-12 20:42:22 -0400

    3-3 Almost complete utility system.

--- src/utility/ChessValueUtil.java
+++ src/utility/ChessValueUtil.java
@@ -3,92 +3,158 @@ package utility;
 import components.BlackOrWhite;
 import components.Pos;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 public class ChessValueUtil {
-    private static final HashMap<String, Integer> blackChessLinkToValue = new HashMap<>();
-    private static final HashMap<String, Integer> whiteChessLinkToValue = new HashMap<>();
+    private static final HashMap<String, Integer> blackChessChainToValue = new HashMap<>();
+    private static final HashMap<String, Integer> whiteChessChainToValue = new HashMap<>();
 
     static {
-        blackChessLinkToValue.put("#####", 100000);
-        blackChessLinkToValue.put("+####+", 5000);
-        blackChessLinkToValue.put("#+###", 700);
-        blackChessLinkToValue.put("###+#", 700);
-        blackChessLinkToValue.put("*####+", 1000);
-        blackChessLinkToValue.put("+####*", 1000);
-        blackChessLinkToValue.put("##+##", 700);
-        blackChessLinkToValue.put("+###+", 500);
-        blackChessLinkToValue.put("#+##", 150);
-        blackChessLinkToValue.put("##+#", 150);
-        blackChessLinkToValue.put("++###*", 100);
-        blackChessLinkToValue.put("*###++", 100);
-        blackChessLinkToValue.put("+#+##*", 80);
-        blackChessLinkToValue.put("*##+#+", 80);
-        blackChessLinkToValue.put("+##+#*", 60);
-        blackChessLinkToValue.put("*#+##+", 60);
-        blackChessLinkToValue.put("#++##", 60);
-        blackChessLinkToValue.put("##++#", 60);
-        blackChessLinkToValue.put("#+#+#", 60);
-        blackChessLinkToValue.put("*+###+*", 60);
-        blackChessLinkToValue.put("++##++", 50);
-        blackChessLinkToValue.put("+#+#+", 20);
-        blackChessLinkToValue.put("#++#", 20);
-        blackChessLinkToValue.put("+++##*", 10);
-        blackChessLinkToValue.put("*##+++", 10);
-        blackChessLinkToValue.put("++#+#*", 10);
-        blackChessLinkToValue.put("*#+#++", 10);
-        blackChessLinkToValue.put("+#++#*", 10);
-        blackChessLinkToValue.put("*#++#+", 10);
-        blackChessLinkToValue.put("#+++#", 10);
+        blackChessChainToValue.put("#####", 100000);
+        blackChessChainToValue.put("+####+", 5000);
+        blackChessChainToValue.put("#+###", 700);
+        blackChessChainToValue.put("###+#", 700);
+        blackChessChainToValue.put("*####+", 1000);
+        blackChessChainToValue.put("+####*", 1000);
+        blackChessChainToValue.put("##+##", 700);
+        blackChessChainToValue.put("+###+", 500);
+        blackChessChainToValue.put("#+##", 150);
+        blackChessChainToValue.put("##+#", 150);
+        blackChessChainToValue.put("++###*", 100);
+        blackChessChainToValue.put("*###++", 100);
+        blackChessChainToValue.put("+#+##*", 80);
+        blackChessChainToValue.put("*##+#+", 80);
+        blackChessChainToValue.put("+##+#*", 60);
+        blackChessChainToValue.put("*#+##+", 60);
+        blackChessChainToValue.put("#++##", 60);
+        blackChessChainToValue.put("##++#", 60);
+        blackChessChainToValue.put("#+#+#", 60);
+        blackChessChainToValue.put("*+###+*", 60);
+        blackChessChainToValue.put("++##++", 50);
+        blackChessChainToValue.put("+#+#+", 20);
+        blackChessChainToValue.put("#++#", 20);
+        blackChessChainToValue.put("+++##*", 10);
+        blackChessChainToValue.put("*##+++", 10);
+        blackChessChainToValue.put("++#+#*", 10);
+        blackChessChainToValue.put("*#+#++", 10);
+        blackChessChainToValue.put("+#++#*", 10);
+        blackChessChainToValue.put("*#++#+", 10);
+        blackChessChainToValue.put("#+++#", 10);
 
-        whiteChessLinkToValue.put("*****", 10000);
-        whiteChessLinkToValue.put("+****+", 5000);
-        whiteChessLinkToValue.put("*+***", 700);
-        whiteChessLinkToValue.put("***+*", 700);
-        whiteChessLinkToValue.put("+****#", 1000);
-        whiteChessLinkToValue.put("#****+", 1000);
-        whiteChessLinkToValue.put("**+**", 700);
-        whiteChessLinkToValue.put("+***+", 500);
-        whiteChessLinkToValue.put("*+**", 150);
-        whiteChessLinkToValue.put("**+*", 150);
-        whiteChessLinkToValue.put("++***#", 100);
-        whiteChessLinkToValue.put("#***++", 100);
-        whiteChessLinkToValue.put("+*+**#", 80);
-        whiteChessLinkToValue.put("#**+*+", 80);
-        whiteChessLinkToValue.put("+**+*#", 60);
-        whiteChessLinkToValue.put("#*+**+", 60);
-        whiteChessLinkToValue.put("*++**", 60);
-        whiteChessLinkToValue.put("**++*", 60);
-        whiteChessLinkToValue.put("*+*+*", 60);
-        whiteChessLinkToValue.put("#+***+#", 60);
-        whiteChessLinkToValue.put("++**++", 50);
-        whiteChessLinkToValue.put("+*+*+", 20);
-        whiteChessLinkToValue.put("*++*", 20);
-        whiteChessLinkToValue.put("+++**#", 10);
-        whiteChessLinkToValue.put("#**+++", 10);
-        whiteChessLinkToValue.put("++*+*#", 10);
-        whiteChessLinkToValue.put("#*+*++", 10);
-        whiteChessLinkToValue.put("+*++*#", 10);
-        whiteChessLinkToValue.put("#*++*+", 10);
-        whiteChessLinkToValue.put("*+++*", 10);
+        whiteChessChainToValue.put("*****", 10000);
+        whiteChessChainToValue.put("+****+", 5000);
+        whiteChessChainToValue.put("*+***", 700);
+        whiteChessChainToValue.put("***+*", 700);
+        whiteChessChainToValue.put("+****#", 1000);
+        whiteChessChainToValue.put("#****+", 1000);
+        whiteChessChainToValue.put("**+**", 700);
+        whiteChessChainToValue.put("+***+", 500);
+        whiteChessChainToValue.put("*+**", 150);
+        whiteChessChainToValue.put("**+*", 150);
+        whiteChessChainToValue.put("++***#", 100);
+        whiteChessChainToValue.put("#***++", 100);
+        whiteChessChainToValue.put("+*+**#", 80);
+        whiteChessChainToValue.put("#**+*+", 80);
+        whiteChessChainToValue.put("+**+*#", 60);
+        whiteChessChainToValue.put("#*+**+", 60);
+        whiteChessChainToValue.put("*++**", 60);
+        whiteChessChainToValue.put("**++*", 60);
+        whiteChessChainToValue.put("*+*+*", 60);
+        whiteChessChainToValue.put("#+***+#", 60);
+        whiteChessChainToValue.put("++**++", 50);
+        whiteChessChainToValue.put("+*+*+", 20);
+        whiteChessChainToValue.put("*++*", 20);
+        whiteChessChainToValue.put("+++**#", 10);
+        whiteChessChainToValue.put("#**+++", 10);
+        whiteChessChainToValue.put("++*+*#", 10);
+        whiteChessChainToValue.put("#*+*++", 10);
+        whiteChessChainToValue.put("+*++*#", 10);
+        whiteChessChainToValue.put("#*++*+", 10);
+        whiteChessChainToValue.put("*+++*", 10);
     }
 
-    public static List<List<String>> getSettedLocationList(String[][] board, BlackOrWhite currentPlayer,
-                                                           Pos pos, int length, int loc) {
+    public static Double[][] getChessValueBoard(String[][] board, BlackOrWhite currentPlayer) {
+        Double[][] chessValueBoard = new Double[board.length][board[0].length];
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                chessValueBoard[row][col] = getPosValue(board, currentPlayer, new Pos(row, col));
+            }
+        }
+
+        return chessValueBoard;
+    }
+
+    private static double getPosValue(String[][] board, BlackOrWhite currentPlayer, Pos pos) {
+        if (!Objects.equals(board[pos.row][pos.col], "+")) {
+            return -1;
+        } else {
+            double posValue = 0.0;
+
+            List<String> stringChessChain = getStringChessChain(board, currentPlayer, pos);
+
+            for (String chain : stringChessChain) {
+                int attackScore = currentPlayer == BlackOrWhite.BLACK ?
+                        blackChessChainToValue.getOrDefault(chain, 0) :
+                        whiteChessChainToValue.getOrDefault(chain, 0);
+                int defenseScore = currentPlayer == BlackOrWhite.BLACK ?
+                        whiteChessChainToValue.getOrDefault(chain, 0) :
+                        blackChessChainToValue.getOrDefault(chain, 0);
+                posValue+=attackScore * 0.7 + defenseScore * 0.3;
+            }
+
+            return posValue;
+        }
+    }
+
+    private static int getChainScore(String chain, HashMap<String, Integer> chainToValue) {
+        return chainToValue.getOrDefault(chain, 0);
+    }
+
+    private static List<String> getStringChessChain(String[][] board, BlackOrWhite currentPlayer, Pos pos) {
+        List<String> stringChessChains = new ArrayList<>();
+        List<List<String>> allChessChains = getAllChessChains(board, currentPlayer, pos);
+
+        for (List<String> chain : allChessChains) {
+            stringChessChains.add(chessChainToString(chain));
+        }
+
+        return stringChessChains;
+    }
+
+    private static String chessChainToString(List<String> list) {
+        StringBuilder pool = new StringBuilder();
+        for (String chessColor : list) {
+            pool.append(chessColor);
+        }
+        return pool.toString();
+    }
+
+    private static List<List<String>> getAllChessChains(String[][] board, BlackOrWhite currentPlayer, Pos pos) {
+        List<List<String>> chessChain = new ArrayList<>();
+
+        for (int i = 4; i <= 7; i++) {
+            for (int j = 1; j <= i; j++) {
+                List<List<String>> singleChessChainList = getLocationList(board, currentPlayer, pos, i, j);
+                if (!singleChessChainList.isEmpty()) chessChain.addAll(singleChessChainList);
+            }
+        }
+
+        return chessChain;
+    }
+
+    private static List<List<String>> getLocationList(String[][] board, BlackOrWhite currentPlayer,
+                                                      Pos pos, int chainLength, int loc) {
         int row = pos.row;
         int col = pos.col;
 
-        List<List<String>> list = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
 
         String currentPlayerChess = currentPlayer == BlackOrWhite.BLACK ? "#" : "*";
         String opponentPlayerChess = currentPlayer == BlackOrWhite.BLACK ? "*" : "#";
 
         Pos layStart = new Pos(row, col - loc + 1);
-        Pos layEnd = new Pos(row, col + length - loc);
+        Pos layEnd = new Pos(row, col + chainLength - loc);
         int LayStartCol = layStart.col;
         int LayEndCol = layEnd.col;
 
@@ -112,12 +178,12 @@ public class ChessValueUtil {
                 }
             }
 
-            list.add(attackList);
-            list.add(defendList);
+            result.add(attackList);
+            result.add(defendList);
         }
 
         Pos standStart = new Pos(row - loc + 1, col);
-        Pos standEnd = new Pos(row + length - loc, col);
+        Pos standEnd = new Pos(row + chainLength - loc, col);
         int StandStartRow = standStart.row;
         int StandEndRow = standEnd.row;
 
@@ -139,12 +205,12 @@ public class ChessValueUtil {
                     defendList.add(board[i][col]);
                 }
             }
-            list.add(attackList);
-            list.add(defendList);
+            result.add(attackList);
+            result.add(defendList);
         }
 
         Pos MainStart = new Pos(row - loc + 1, col - loc + 1);
-        Pos MainEnd = new Pos(row + length - loc, col + length - loc);
+        Pos MainEnd = new Pos(row + chainLength - loc, col + chainLength - loc);
 
         int MainStartRow = MainStart.row;
         int MainEndRow = MainEnd.row;
@@ -168,12 +234,12 @@ public class ChessValueUtil {
                     defendList.add(board[rowi][coli]);
                 }
             }
-            list.add(attackList);
-            list.add(defendList);
+            result.add(attackList);
+            result.add(defendList);
         }
 
         Pos ViceStart = new Pos(row + loc - 1, col - loc + 1);
-        Pos ViceEnd = new Pos(row - length + loc, col + length - loc);
+        Pos ViceEnd = new Pos(row - chainLength + loc, col + chainLength - loc);
 
         int ViceStartRow = ViceStart.row;
         int ViceEndRow = ViceEnd.row;
@@ -199,15 +265,15 @@ public class ChessValueUtil {
                 }
             }
 
-            list.add(attackList);
-            list.add(defendList);
+            result.add(attackList);
+            result.add(defendList);
         }
 
-        return list;
+        return result;
     }
 
     private static boolean inBoard(Pos p) {
-        return p.row >= 0 && p.row < 15 && p.col >= 0 && p.col < 15;
+        return p.row >= 0 && p.row < 7 && p.col >= 0 && p.col < 7;
     }
 
     private static boolean assignPositionValue(Pos chessPosition, List<String> chessLink, String[][] board) {
@@ -215,8 +281,8 @@ public class ChessValueUtil {
 
         if (!Objects.equals(board[chessPosition.row][chessPosition.col], "+")) return false;
         for (String s : chessLink) {
-            if (blackChessLinkToValue.containsKey(s)) {
-                value += blackChessLinkToValue.get(s);
+            if (blackChessChainToValue.containsKey(s)) {
+                value += blackChessChainToValue.get(s);
             }
         }
 
